ac6a8881fde0d692af79bb5bb17e0cec26845c16
Valera Drokin <drokin.valera@gmail.com>
2022-10-24 23:14:44 +0300

    Init project

---
+++ src/main/java/com/github/kolizey72/riverbank/RiverBankApplication.java
@@ -0,0 +1,13 @@
+package com.github.kolizey72.riverbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiverBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiverBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/kolizey72/riverbank/RiverBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kolizey72.riverbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiverBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b44033367d7dff0eb40dcab7f3adece8f965d1a5
Valera Drokin <drokin.valera@gmail.com>
2022-11-07 02:53:31 +0300

    Add Bootstrap and improve auth pages appearance

--- src/main/java/com/github/kolizey72/riverbank/config/SecurityConfig.java
+++ src/main/java/com/github/kolizey72/riverbank/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/auth/**", "/css/**", "/js/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
